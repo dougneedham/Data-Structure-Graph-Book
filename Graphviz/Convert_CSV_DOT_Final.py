db2169323a8adb10d5d29e8cf15cd7f79e762366
unknown <dneedham@OHCINDNEEDHA-L1.corp.local>
2015-03-16 08:22:55 -0400

    Added code for Graphviz

---
+++ Graphviz/Convert_CSV_DOT_Final.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+from random import randint
+import sys
+
+csv_file = sys.argv[1]
+dot_file = sys.argv[2]
+
+
+csv_input = open(csv_file,'r')
+dot_output = open(dot_file,'w+')
+
+app_list = []
+entity_list = []
+
+dot_output.write("digraph dsgl2 { \n")
+dot_output.write("rankdir=LR;\n")
+for data in csv_input:
+    (Source,Target,Label) =  data.strip().split(',')
+
+    if(Source != 'Source'):
+        dot_output.write("\"%s\" -> \"%s\" [ label = \"%s\" ];\n" % (Source,Target,Label))
+dot_output.write("}\n")
+dot_output.close()
