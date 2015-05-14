bea80ea5d0c3224087bec9db3a82e5531158c058
unknown <dneedham@OHCINDNEEDHA-L1.corp.local>
2015-05-14 09:20:41 -0400

    Added argument and logging options

--- AppendixA/Add_Time_Column_To_csv.py
+++ AppendixA/Add_Time_Column_To_csv.py
@@ -3,30 +3,68 @@ from __future__ import division
 import sys
 import os
 import csv
+import argparse
+import logging
+import logging.config
+sys.path.append(".")
+import properties
+
 #
 # This code adds the time column as the last column of a CSV file that is to be analyzed. 
 # Argument 1 is the input file 
 # Argument 2 is the output file 
 # argument 3 is the length of the grouping for the time argument
-filename = sys.argv[1]
-outfile  = sys.argv[2]
-grouping = sys.argv[3]
-grouping = int(grouping)
-index_row = 0
-time = 0
-outfh = open(outfile,'wb')
-outwriter = csv.writer(outfh,delimiter = '|')
-with open(filename,'r') as csvfile:
-	read_data = csv.reader(csvfile,delimiter='|',quotechar='"')
-	for row in read_data:
-		if (index_row % grouping) == 0:
-			time = time + 1
-		if index_row == 0:
-			row.append('Time')
-		else:	
-			row.append(str(time))
-		outwriter.writerow(row)
-		#print(','.join(row))
-		index_row = index_row + 1
-outfh.close()
-csvfile.close()
+
+lgr = logging.Logger
+
+def init_logger(log_level):
+    # configure logging
+    if log_level is None:
+        log_level = properties.log_level
+        
+    logging.config.fileConfig("logconfig.ini")
+    global lgr
+    lgr = logging.getLogger(log_level)
+
+def main():
+    parser = argparse.ArgumentParser(description='Add the Time Column to a normal CSV file');
+    parser.add_argument("-i", "--infile", help="Input File",required=True)
+    parser.add_argument("-o", "--outfile", help="Output File",required=True)
+    parser.add_argument("-g", "--grouping", help="Grouping\nNumber of rows that will be grouped together with an integer",type=int,default=50)
+    parser.add_argument("-q","--quote", help = "Quotecharacter to use",default='"')
+    parser.add_argument("-d","--delimiter", help = "Delimiter to use",default=',')
+    parser.add_argument("-ll", "--loglevel",
+                        help="Log Level; 'log_error' logs only error messages to file; 'logs/EntityAnalysisDebug.log' logs all messages to file");
+    args = parser.parse_args();
+    init_logger(args.loglevel)
+    process_file(args.infile,args.outfile,args.grouping,args.delimiter,args.quote)
+
+def process_file(infile_arg,outfile_arg,grouping_arg,delimiter_arg,quotechar_arg):
+	outfile  = outfile_arg
+	grouping = grouping_arg
+	lgr.info("Input  file is: {0}".format(infile_arg))
+	lgr.info("Output file is: {0}".format(outfile_arg))
+	lgr.info("Grouping    is: {0}".format(grouping_arg))
+	lgr.info("Delimiter    is: {0}".format(delimiter_arg))
+
+	index_row = 0
+	time = 0
+	outfh =	 open(outfile,'wb')
+	outwriter = csv.writer(outfh,delimiter=delimiter_arg)
+	with open(infile_arg,'r') as csvfile:
+		read_data = csv.reader(csvfile,delimiter=delimiter_arg,quotechar=quotechar_arg)
+		for row in read_data:
+			if (index_row % grouping) == 0:
+				time = time + 1
+			if index_row == 0:
+				row.append('Time')
+			else:		
+				row.append(str(time))
+			outwriter.writerow(row)
+			#print(','.join(row))
+			index_row = index_row + 1
+	outfh.close()
+	csvfile.close()
+
+if __name__ == '__main__':
+    main()
---
+++ AppendixA/properties.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+## PROPERTIES for this run ##
+# log_level = None           # log to root  > console only
+#log_level = "log_error"     # log to file framework_err.log; level: warning, error, critical
+log_level = "log_error.log_debug"     # log to files
